45e4eab847fdddf368fdb28f60a45bb0abc0fa2c
luiscarlosrojastorres <redtower.soft@gmail.com>
2022-10-26 05:14:13 -0300

    Added About views

--- SACnet/SACnet/Controllers/HomeController.cs
+++ SACnet/SACnet/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace SACnet.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult About()
         {
             return View();
         }
